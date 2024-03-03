20965dfd9a109a6302730ddec3deafe9257fbe43
SallesThe <manodada26@gmail.com>
2024-03-02 23:14:06 -0300

    changing blog file

--- resources/views/mobile-blog/index.blade.php
+++ resources/views/mobile-blog/index.blade.php
@@ -4,4 +4,11 @@
 <header>
     <img src="{{ asset('images/Logo.png') }}" alt="Logomark of Simetra" srcset="">
 </header>
+<div class="title_page">
+    <h1>Blog</h1>
+    <div class="orange_bar"></div>
+</div>
+<footer>
+    <p>© 2021  Simetra - Laboratório Veterinário. Todos os direitos reservados.</p>
+</footer>
 @endsection
